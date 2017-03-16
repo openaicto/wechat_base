b1e45c98e1bbb3e3472e7dbd6f2e488596488fdb
wangshudong <wsd312@163.com>
2017-03-16 11:12:10 +0800

    add packge

---
+++ oauth2/db.php
@@ -0,0 +1,12 @@
+<?php
+$dbms='mysql';     //数据库类型
+$host='192.168.99.100'; //数据库主机名
+$dbName='web';    //使用的数据库
+$user='web';      //数据库连接用户名
+$pass='web';          //对应的密码
+$dbport = '3304';  //端口
+$dsn="$dbms:host=$host;port={$dbport};dbname=$dbName";
+
+
+
+
---
+++ oauth2/index.php
@@ -0,0 +1,2 @@
+<meta charset="utf-8">
+<a href="oauth2.php">授权</a>
