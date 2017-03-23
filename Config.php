97a61d0692e15060040b511ef55a01b89a734ef1
soulsq <464538576@qq.com>
2017-03-23 17:50:17 +0800

    update app config

--- Config.php
+++ Config.php
@@ -6,7 +6,7 @@
 $basedir = dirname(__FILE__); 
 class Config
 {	
-    // const APPID = 'wx663f128c36e92c8f';
-    // const APPSECRET = '70481d36bbd3a680f91be38a752759c8';
+    const APPID = 'wx663f128c36e92c8f';
+    const APPSECRET = '70481d36bbd3a680f91be38a752759c8';
     const TOKEN = 'someetweixin';
 }
